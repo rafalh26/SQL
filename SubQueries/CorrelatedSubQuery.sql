2f70ceb0f3d000ccaa7493813a2541cd412b6491
Rafal <76202803+rafalh26@users.noreply.github.com>
2024-11-26 07:41:17 +0100

    Update CorrelatedSubQuery.sql

--- SubQueries/CorrelatedSubQuery.sql
+++ SubQueries/CorrelatedSubQuery.sql
@@ -1,1 +1,7 @@
-select
+SELECT product_id, product_name, category_id, list_price
+FROM oes.products o
+WHERE list_price = (
+    SELECT MIN(list_price)
+    FROM oes.products i
+    WHERE i.category_id = o.category_id
+);
